2b9f2a5bb974a6df25d9bce78988ce7c9eec8bc3
Egor Lukyanchenko <13577050+egorxe@users.noreply.github.com>
2025-12-14 17:44:25 +0500

    Update Caravel

--- caravel/sim/caravel_tb/uart_tb.v
+++ caravel/sim/caravel_tb/uart_tb.v
@@ -27,113 +27,113 @@
 
 
 module uart_tb;
-	reg clock;
-	reg RSTB;
-	reg power1;
+    reg clock;
+    reg RSTB;
+    reg power1;
 
-	wire gpio;
+    wire gpio;
     wire [15:0] checkbits;
-	wire [37:0] mprj_io;
-	wire flash_csb;
-	wire flash_clk;
-	wire flash_io0;
-	wire flash_io1;
-	wire uart_tx;
+    wire [37:0] mprj_io;
+    wire flash_csb;
+    wire flash_clk;
+    wire flash_io0;
+    wire flash_io1;
+    wire uart_tx;
     reg uart_rx = 1'b1;
-	wire uart_loopback;
-	wire SDO;
+    wire uart_loopback;
+    wire SDO;
     
     reg test_success = 1'b0;
 
-	assign checkbits = mprj_io[31:16];
-	assign uart_tx = mprj_io[6];
-	assign mprj_io[5] = uart_rx;
+    assign checkbits = mprj_io[31:16];
+    assign uart_tx = mprj_io[6];
+    assign mprj_io[5] = uart_rx;
 
-	always #25 clock <= (clock === 1'b0);
+    always #25 clock <= (clock === 1'b0);
 
-	initial begin
-		clock = 0;
-	end
-	
-	`define CHIP_TOP_HIER uut.chip
+    initial begin
+        clock = 0;
+    end
+    
+    `define CHIP_TOP_HIER uut.chip
     `include "sdf.vh"
 
-	initial begin
-		$display("UART Test started");
+    initial begin
+        $display("UART Test started");
         
-		repeat (450) begin
-			repeat (1000) @(posedge clock);
-			// Diagnostic. . . interrupts output pattern.
-		end
+        repeat (450) begin
+            repeat (1000) @(posedge clock);
+            // Diagnostic. . . interrupts output pattern.
+        end
         $display("%c[1;31m",27);
-		`ifdef GL
-			$display ("Monitor: Timeout, Test UART (GL) Failed");
-		`else
-			$display ("Monitor: Timeout, Test UART (RTL) Failed");
-		`endif
-		$display("%c[0m",27);
-		$finish;
-	end
-
-	initial begin
-		RSTB <= 1'b0;
-		#1000;
-		RSTB <= 1'b1;	    // Release reset
-		#2000;
-	end
-
-	initial begin		// Power-up sequence
-		power1 <= 1'b0;
-		#2000;
-		power1 <= 1'b1;
-	end
-
-	always @(gpio) begin
-		if(gpio == 1'b1) begin
-			`ifdef GL
-				$display("UART Test (GL) finished");
-			`else
-				$display("UART Test (RTL) finished");
-			`endif
-			test_success <= 1'b1;
-		end
-	end
-
-	wire VDD5V0;
-	wire VSS;
-
-	assign VDD5V0 = power1;
-	assign VSS = 1'b0;
-	
-	assign mprj_io[3] = 1'b1;  // Force CSB high.
-	assign mprj_io[0] = 1'b0;  // Disable debug mode
-
-	caravel uut (
-        `ifdef USE_POWER_PINS
-		.VDD	  (VDD5V0),
-		.VSS	  (VSS),
+        `ifdef GL
+            $display ("Monitor: Timeout, Test UART (GL) Failed");
+        `else
+            $display ("Monitor: Timeout, Test UART (RTL) Failed");
         `endif
-		.clock	  (clock),
-		.gpio     (gpio),
-		.mprj_io  (mprj_io),
-		.flash_csb(flash_csb),
-		.flash_clk(flash_clk),
-		.flash_io0(flash_io0),
-		.flash_io1(flash_io1),
-		.resetb	  (RSTB)
-	);
+        $display("%c[0m",27);
+        $finish;
+    end
 
-	spiflash #(
-		.FILENAME({`HEX_PREFIX, "uart.hex"})
-	) spiflash (
-		.csb(flash_csb),
-		.clk(flash_clk),
-		.io0(flash_io0),
-		.io1(flash_io1),
-		.io2(),			// not used
-		.io3()			// not used
-	);
+    initial begin
+        RSTB <= 1'b0;
+        #1000;
+        RSTB <= 1'b1;        // Release reset
+        #2000;
+    end
 
-		
+    initial begin        // Power-up sequence
+        power1 <= 1'b0;
+        #2000;
+        power1 <= 1'b1;
+    end
+
+    always @(gpio) begin
+        if(gpio == 1'b1) begin
+            `ifdef GL
+                $display("UART Test (GL) finished");
+            `else
+                $display("UART Test (RTL) finished");
+            `endif
+            test_success <= 1'b1;
+        end
+    end
+
+    wire VDD5V0;
+    wire VSS;
+
+    assign VDD5V0 = power1;
+    assign VSS = 1'b0;
+    
+    assign mprj_io[3] = 1'b1;  // Force CSB high.
+    assign mprj_io[0] = 1'b0;  // Disable debug mode
+
+    caravel uut (
+        `ifdef USE_POWER_PINS
+        .VDD      (VDD5V0),
+        .VSS      (VSS),
+        `endif
+        .clock      (clock),
+        .gpio     (gpio),
+        .mprj_io  (mprj_io),
+        .flash_csb(flash_csb),
+        .flash_clk(flash_clk),
+        .flash_io0(flash_io0),
+        .flash_io1(flash_io1),
+        .resetb      (RSTB)
+    );
+
+    spiflash #(
+        .FILENAME({`HEX_PREFIX, "uart.hex"})
+    ) spiflash (
+        .csb(flash_csb),
+        .clk(flash_clk),
+        .io0(flash_io0),
+        .io1(flash_io1),
+        .io2(),            // not used
+        .io3()            // not used
+    );
+
+        
 endmodule
 `default_nettype wire
